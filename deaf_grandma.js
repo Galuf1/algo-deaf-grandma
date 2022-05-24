276ec518d2248ff048297bd0159ac86cdf971b84
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-24 13:39:14 +0200

    working

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,38 @@
 function deafGrandma() {
 
+    let goodbyes = 0
+    
+    let message = "HEY KID!"
+    while (true) {
+        
+        let talk = prompt(message)
+        
+        
+        if (talk=== ""){
+            message = "WHAT?!";
+        } else if (talk === "GOODBYE!") {
+            if (goodbyes < 1) {
+                message ="LEAVING SO SOON?"
+                goodbyes++
+            } else if (goodbye < 2) {
+                message = "LATER, SKATER!"
+                break;
+            }
+            
+        } else if (talk === talk.toUpperCase()) {
+            message = "NO, NOT SINCE 1946!"
+        } else {
+            message = "SPEAK UP, KID!"
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+
 }
 
 deafGrandma();
