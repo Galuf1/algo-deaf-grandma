76ef90f278915117d19c6f37a8aa057a250803d7
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-25 20:48:11 +0200

    translated from js

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,4 +1,23 @@
 def deaf_grandma():
+    counter = 0
+    message = "HEY KID!"
+
+    while True:
+        talk = input(message + "\n")
+        if talk == "":
+            message = "WHAT?!"
+        elif talk == "GOODBYE!":
+            if counter == 0:
+                message = "LEAVING SO SOON?"
+                counter +=1
+            elif counter == 1:
+                message = "LATER, SKATER!"
+                break
+        elif talk == talk.upper():
+            message = "NO, NOT SINCE 1946!"
+        else: 
+            message = "SPEAK UP, KID!"
+            
 
 
 
